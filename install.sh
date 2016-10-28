2d8e04df33506d36b45b3d8667d3b440daf507bd
Peter Stevens <peter.stevens@bakerhughes.com>
2016-10-28 16:17:08 +0200

    Release 4.0

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -x
 DEST='C:/Program Files/sonarqube-5.6/extensions/plugins'
 SRC=target
-PLUGIN='sonar-resharper-plugin*.jar'
+PLUGIN='sonar-resharper-plugin-*.jar'
 rm "$DEST"/$PLUGIN
 cp $SRC/$PLUGIN "$DEST"
